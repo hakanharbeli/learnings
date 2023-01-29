4a736b9e30e884784d0da9b7e762d76cbaf7de4b
hakanharbeli <zh162014>
2023-01-29 09:20:32 +0300

    dogrulamguncellendi

--- contracts/sample1_dogrulama.sol
+++ contracts/sample1_dogrulama.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.8.0;
 //SPDX-License-Identifier: Unlicensed
 
 contract Dogrulama{
-    
+
+    //bir değişken constant ya da immutable olarak işaretlenirse bu değişken bir daha değiştirlemez.
+    uint public constant karsilama =5;
     //fonksiyon tanımladık önce fonksiyon adı, public-private görünürlük , pure= blockchain'e gönderme,
     //returns = döndürülecek değer tipi
     function dogrula(uint number) public pure returns(string memory sonuc){
-        uint karsilama =5;
+        
         if(karsilama == number){
 
             sonuc="Dogrulandi...";
