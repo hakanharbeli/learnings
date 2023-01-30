0db7b579527cf3f5e60b5a4d153fcd6c753c5d34
hakanharbeli <zh162014>
2023-01-30 21:21:58 +0300

    struct

--- contracts/sample3_struct.sol
+++ contracts/sample3_struct.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 //SPDX-License-Identifier: Unlicensed
 
 contract Structer{
-
+//Yapılar (structs), nesneye yönelik programlama dillerindeki sınıflarla (classes) benzer işleve sahiptir
+// Diğer dillerde class olarak isimlendirilen sınıflar Solidity'de struct olarak isimlendirilir.
     struct Order{
         address costumer;
         string zipCoder;
